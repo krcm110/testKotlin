c8359463aee1d0a7f54a5bb8898ac47cba00ee75
krcm110 <14064635@qq.com>
2018-09-29 23:04:17 +0800

    add background of launch activity

--- app/src/main/java/com/example/krcm110/myapplication/com/base/BaseActivity.kt
+++ app/src/main/java/com/example/krcm110/myapplication/com/base/BaseActivity.kt
@@ -1,6 +1,7 @@
 package com.example.krcm110.myapplication.com.base
 
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -17,13 +18,25 @@ abstract class BaseActivity: AppCompatActivity()
      */
     protected var mLayoutStatusView: MultipleStatusView? = null
 
+    /**
+     * 沉浸式模式
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            val decorView = window.decorView
+            decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layoutId())
-
-
-
         initData()
         initView()
         start()
